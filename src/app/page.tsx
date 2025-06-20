a9e0f8b2a4d7e3f3b95aff64e0a33002d9dea68e
doruletzz <dorosdorulucian@gmail.com>
2025-06-20 19:40:55 +0300

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect('/home');
+
   return (
     <div className="flex">
       <h1 className="text-2xl font-bold">
